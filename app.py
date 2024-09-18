92011cd939fab467d10a1ec31239035ddbf2f5ff
unknown <akinolaayomide226@gmail.com>
2024-09-18 16:30:59 +0100

    Latest

--- app.py
+++ app.py
@@ -6,7 +6,26 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import LancasterStemmer
 import json
 
+# Define the ChatModel class
+class ChatModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(ChatModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return self.softmax(x)
+
 # Load the model and data
+input_size = len(pickle.load(open('all_words.pkl', 'rb')))
+hidden_size = 8
+output_size = len(pickle.load(open('classes.pkl', 'rb')))
+
 model = ChatModel(input_size, hidden_size, output_size)
 model.load_state_dict(torch.load('chat_model.pth'))
 model.eval()
